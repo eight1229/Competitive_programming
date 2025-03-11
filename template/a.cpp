921076bc7418d0ff1c944a449b3587f684e273e4
eight1229 <takakota9320@gmail.com>
2025-03-12 00:27:40 +0900

    Fix template

--- template/a.cpp
+++ template/a.cpp
@@ -1,6 +1,6 @@
 #include "common.cpp"
-using mint =modint1000000007;
-// using mint =modint998244353;
+// using mint =modint1000000007;
+using mint =modint998244353;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
